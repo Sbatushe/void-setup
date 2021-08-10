0f2a80cdf37fd79e5d748267de27275a675885d1
Emanuele Sabato <44072381+Sbatushe@users.noreply.github.com>
2021-08-10 02:22:06 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,6 +15,14 @@ chsh -s /bin/fish
 
 #scorciatoie fish?
 echo "[fish setup]"
+cp fish-functions/battery.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/emerge.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/lum.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/tutorial.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/uni.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/xi.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/xq.fish /home/sbatushe/.config/fish/functions/
+cp fish-functions/xr.fish /home/sbatushe/.config/fish/functions/
 
 #micro
 echo "[installing micro]"
@@ -26,7 +34,7 @@ sudo mv micro /usr/bin/
 #doas
 echo "[installing doas]"
 sudo xbps-install opendoas
-sudo mv doas.conf /etc/
+sudo cp doas.conf /etc/
 
 #sudo micro /etc/doas.conf
 #permit nopass sbatushe as root
@@ -39,5 +47,13 @@ doas xbps-install mc neofetch htop flatpak zip unzip tlp
 #GUI
 echo "[GUI setup]"
 doas xbps-install sway evince mirage alacritty
+
+#alacritty
+echo "[alacritty setup]"
+mkdir /home/sbatushe/alacritty
+cp alacritty.yml /home/sbatushe/alacritty/
+
+#flatpaks
+
 
 xbps-remove -oO #apt autoremove
