d4ff5ddf0f373312d84641dbcd847330a4cd4645
Emanuele Sabato <44072381+Sbatushe@users.noreply.github.com>
2021-09-09 21:20:04 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 echo "[installing fish shell]";
 sudo xbps-install -y fish-shell;
 chsh -s /bin/fish;
+echo "OK";
 
 #scorciatoie fish?
 echo "[fish setup]";
@@ -18,6 +19,7 @@ cp fish-functions/uni.fish /home/sbatushe/.config/fish/functions/;
 cp fish-functions/xi.fish /home/sbatushe/.config/fish/functions/;
 cp fish-functions/xq.fish /home/sbatushe/.config/fish/functions/;
 cp fish-functions/xr.fish /home/sbatushe/.config/fish/functions/;
+echo "OK";
 
 #micro
 echo "[installing micro]";
@@ -25,29 +27,33 @@ sudo xbps-install -y curl;
 curl https://getmic.ro | bash;
 sudo mv micro /usr/bin/;
 # entra in micro ed usa Ctrl+e, poi set colorscheme simple
+echo "OK";
 
 #doas
 echo "[installing doas]";
 sudo xbps-install -y opendoas;
 sudo cp doas.conf /etc/;
-
 #sudo micro /etc/doas.conf
 #permit nopass sbatushe as root
+echo "OK";
 
-#altro
-echo "[installing misc]";
-sudo xbps-install -y mc neofetch htop flatpak zip unzip tlp;
-
-#GUI
-echo "[GUI setup]";
-sudo xbps-install -y sway evince mirage alacritty;
+#misc
+echo "[installing misc programs]";
+sudo xbps-install -y mc neofetch htop flatpak zip unzip tlp evince mirage alacritty gnome-40.0_2 mesa-dri terminus-font;
 
 #alacritty
 echo "[alacritty setup]";
 mkdir /home/sbatushe/alacritty;
 cp alacritty.yml /home/sbatushe/alacritty/;
+echo "OK";
 
 #flatpaks
 
-
 sudo xbps-remove -oO; #apt autoremove
+
+#services and GUI
+echo '[GUI setup]'
+sudo ln -s /etc/sv/NetworkManager /var/service/;
+sudo ln -s /etc/sv/gdm var/service/;
+sudo ln -s /etc/sv/dbus /var/service/;
+sudo ln -s /etc/sv/bluetoothd /var/service/;
