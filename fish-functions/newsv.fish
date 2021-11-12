bf356ea243799b97f7791fa5a1470c56366e9c0e
Emanuele Sabato <44072381+Sbatushe@users.noreply.github.com>
2021-11-13 00:50:44 +0100

    T410
    - pulito emerge
    - modificato newsv
    - modificato rmsv
    - forse modificato lssv

--- fish-functions/newsv.fish
+++ fish-functions/newsv.fish
@@ -1,7 +1,7 @@
 function newsv
-    set_color green;  echo -n "* "
-    set_color normal; echo -n "doas ln -s /etc/sv/";
-    set_color green;  echo -n $argv;
-    set_color normal; echo " /var/service/";
+    set_color green;  echo -n "* "; set_color normal; echo -n "doas ln -s /etc/sv/"; 
+    set_color green;  echo -n $argv; set_color normal; echo " /var/service/"; set_color red;
     doas ln -s /etc/sv/$argv /var/service/
+    set_color green;  echo -n "* "; set_color normal; echo "doas sv status "; echo $argv;
+    doas sv status $argv;
 end
--- fish-functions/rmsv.fish
+++ fish-functions/rmsv.fish
@@ -1,6 +1,6 @@
 function rmsv
-    set_color green;  echo -n "* "
-    set_color normal; echo -n "doas rm /var/service/";
-    set_color green;  echo $argv;
+    set_color green;  echo -n "* "; set_color normal; echo -n "doas rm /var/service/"; set_color green;  echo $argv; set_color red;
     doas rm /var/service/$argv;
+    set_color green;  echo -n "* "; set_color normal; echo -n "doas rm /etc/sv/"; set_color green;  echo $argv; set_color red;
+    doas rm /etc/sv/$argv;
 end
